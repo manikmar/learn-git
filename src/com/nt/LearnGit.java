3e5a96021979c87c9ab6d19137e208586f8a5a6f
Mani <manikumar.panithi@gmail.com>
2020-12-20 18:19:14 +0530

    add test() in the main class

--- src/com/nt/LearnGit.java
+++ src/com/nt/LearnGit.java
@@ -3,5 +3,10 @@ package com.nt;
 public class LearnGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        test();
+    }
+
+    public static void test(){
+        System.out.println("test method");
     }
 }
